925d6439a4ed3e098cdc095c0275a95f3b765042
dtchkoidze <dtchkoiddze@gmail.com>
2025-06-21 15:39:37 +0400

    auction-pay-fix

--- app/Models/Auction.php
+++ app/Models/Auction.php
@@ -5,10 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-// app/Models/Auction.php
 class Auction extends Model
 {
     protected $fillable = ['book_id', 'start_price', 'current_price', 'start_time', 'end_time', 'is_active', 'winner_id', 'min_bid', 'max_bid', 'is_free_bid'];
+
 
     public function book()
     {
